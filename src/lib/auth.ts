2149668132242fec587884f4fd4b76e9bade768d
hyepr <hyeprnova@outlook.com>
2025-10-16 19:08:13 +0200

    Better Auth Admin Plugin!

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,13 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; // your drizzle instance
+import { admin } from "better-auth/plugins"
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
     }),
+    plugins: [
+        admin() 
+    ]
 });
